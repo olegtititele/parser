6518cc6ec5dcbbe1606a069b44bfa4d710303df1
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-20 18:54:04 +0300

    Update sqlighter.py

--- sqlite/sqlighter.py
+++ sqlite/sqlighter.py
@@ -75,10 +75,6 @@ class SQLighter:
 # """Действия с бд юзеров"""
 
 
-	def create_users_table(self):
-		users_table = "CREATE TABLE `{}` (`ID` INT AUTO_INCREMENT PRIMARY KEY, `user_id` VARCHAR(255), `username` VARCHAR(255) DEFAULT NULL)".format('users')
-		return self.user_cursor.execute(users_table)
-
 	def add_user(self, user_id, username):
 		sql = "INSERT INTO `users` (`user_id`, `username`) VALUES (%s,%s)"
 		val = user_id, username
@@ -87,63 +83,62 @@ class SQLighter:
 		return
 
 	def check_user(self, user_id):
-		sql = "SELECT * FROM users WHERE user_id = {0}".format(user_id)
-		self.user_cursor.execute(sql)
+		self.user_cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id, ))
 		data = self.user_cursor.fetchone()
 		return data
-		# self.user_mydb.close()
+
 
 	def get_users_data(self):
 		sql = "SELECT * FROM users"
 		self.user_cursor.execute(sql)
 		return self.user_cursor.fetchall()
 
+
 	def get_users_length(self):
 		sql = "SELECT * FROM users"
 		self.user_cursor.execute(sql)
 		length = len(self.user_cursor.fetchall())
 		return length
-	
-	def clear_users_table(self, user_id):
-		sql = "TRUNCATE TABLE `{}`".format(user_id)
-		self.user_cursor.execute(sql)
-		return
+
+	# def clear_users_table(self, user_id):
+	# 	sql = "TRUNCATE TABLE `{}`".format(user_id)
+	# 	self.user_cursor.execute(sql)
+	# 	return
 
 
 
 # """Действия с основной бд"""
 
 	def create_advertisement_table(self, user_id):
-		db_table = "CREATE TABLE `{}` (`Площадка` VARCHAR(255), `Название объявления` VARCHAR(255), `Цена объявления` VARCHAR(255), `Дата создания объявления` VARCHAR(255), `Ссылка на объявление` VARCHAR(255), `Местоположение` VARCHAR(255), `Ссылка на изображение` VARCHAR(255), `Имя продавца` VARCHAR(255), `Номер продавца` VARCHAR(255), `Количество объявлений продавца` VARCHAR(255), `Дата регистрации` VARCHAR(255) DEFAULT NULL, `Бизнесс аккаунт` VARCHAR(255) DEFAULT NULL)".format(user_id)
-		self.adv_cursor.execute(db_table)
+		self.adv_cursor.execute("CREATE TABLE `%s` (`Площадка` VARCHAR(255), `Название объявления` VARCHAR(255), `Цена объявления` VARCHAR(255), `Дата создания объявления` VARCHAR(255), `Ссылка на объявление` VARCHAR(255), `Местоположение` VARCHAR(255), `Ссылка на изображение` VARCHAR(255), `Имя продавца` VARCHAR(255), `Номер продавца` VARCHAR(255), `Количество объявлений продавца` VARCHAR(255), `Дата регистрации` VARCHAR(255) DEFAULT NULL, `Бизнесс аккаунт` VARCHAR(255) DEFAULT NULL)", (user_id))
 		return
 
 
 	def add_advertisement(self, user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business):
-		sql = "INSERT INTO `{}` (`Площадка`, `Название объявления`, `Цена объявления`, `Дата создания объявления`, `Ссылка на объявление`, `Местоположение`, `Ссылка на изображение`, `Имя продавца`, `Номер продавца`, `Количество объявлений продавца`, `Дата регистрации`, `Бизнесс аккаунт`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)".format(user_id)
-		val = platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business
+		sql = "INSERT INTO `%s` (`Площадка`, `Название объявления`, `Цена объявления`, `Дата создания объявления`, `Ссылка на объявление`, `Местоположение`, `Ссылка на изображение`, `Имя продавца`, `Номер продавца`, `Количество объявлений продавца`, `Дата регистрации`, `Бизнесс аккаунт`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+		val = user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business
 		self.adv_cursor.execute(sql, val)
 		self.adv_mydb.commit()
 		return
 
 
 	def get_len_previously_platfrom(self, user_id, platform):
-		sql = "SELECT * FROM `{}` WHERE `Площадка`=%s".format(user_id)
-		adr = (platform, )
+		sql = "SELECT * FROM `%s` WHERE `Площадка`=%s"
+		adr = (user_id, platform, )
 		self.adv_cursor.execute(sql, adr)
 		length = len(self.adv_cursor.fetchall())
 		return length
 
 	def get_previously_adv(self, user_id, platform):
-		sql = "SELECT * FROM `{}` WHERE `Площадка`=%s".format(user_id)
-		adr = (platform, )
+		sql = "SELECT * FROM `%s` WHERE `Площадка`=%s"
+		adr = (user_id, platform, )
 		self.adv_cursor.execute(sql, adr)
 		data = self.adv_cursor.fetchall()
 		return data
 
 	def delete_previously_adv(self, user_id, platform):
-		sql = "DELETE FROM `{}` WHERE `Площадка`=%s".format(user_id)
-		adr = (platform, )
+		sql = "DELETE FROM `%s` WHERE `Площадка`=%s"
+		adr = (user_id, platform, )
 		self.adv_cursor.execute(sql, adr)
 		self.adv_mydb.commit()
 		return 
@@ -151,80 +146,73 @@ class SQLighter:
 
 
 	def len_advertisement_data(self, user_id):
-		sql = "SELECT * FROM `{}`".format(user_id)
-		self.adv_cursor.execute(sql)
+		self.adv_cursor.execute("SELECT * FROM `%s`", (user_id,))
 		length = len(self.adv_cursor.fetchall())
+		print(length)
 		return length
 
 
 	def check_advestisement(self, user_id, adv_url):
-		sql = "SELECT * FROM `{}` WHERE `Ссылка на объявление`=%s".format(user_id)
-		adr = (adv_url, )
+		sql = "SELECT * FROM `%s` WHERE `Ссылка на объявление`=%s"
+		adr = (user_id, adv_url, )
 		self.adv_cursor.execute(sql, adr)
 		data = self.adv_cursor.fetchone()
 		return data
 
 	def get_tel_num(self, user_id, telnum):
-		sql = "SELECT * FROM `{}` WHERE `Номер продавца`=%s".format(user_id)
-		adr = (telnum, )
+		sql = "SELECT * FROM `%s` WHERE `Номер продавца`=%s"
+		adr = (user_id, telnum, )
 		self.adv_cursor.execute(sql, adr)
 		data = self.adv_cursor.fetchone()
 		return data	
 
 	def clear_advestisement(self, user_id):
-		sql = "TRUNCATE TABLE `{}`".format(user_id)
-		return self.adv_cursor.execute(sql)
+		return self.adv_cursor.execute("TRUNCATE TABLE `%s`", (user_id,))
 
 
 # """Действия с временной бд"""
 
 	def create_hash_table(self, tb_name):
-		hash_table = "CREATE TABLE `{}` (`Площадка` VARCHAR(255), `Название объявления` VARCHAR(255), `Цена объявления` VARCHAR(255), `Дата создания объявления` VARCHAR(255), `Ссылка на объявление` VARCHAR(255), `Местоположение` VARCHAR(255), `Ссылка на изображение` VARCHAR(255), `Имя продавца` VARCHAR(255), `Номер продавца` VARCHAR(255), `Количество объявлений продавца` VARCHAR(255), `Дата регистрации` VARCHAR(255) DEFAULT NULL, `Бизнесс аккаунт` VARCHAR(255) DEFAULT NULL)".format(tb_name)
-		self.hash_cursor.execute(hash_table)
+		self.hash_cursor.execute("CREATE TABLE `%s` (`Площадка` VARCHAR(255), `Название объявления` VARCHAR(255), `Цена объявления` VARCHAR(255), `Дата создания объявления` VARCHAR(255), `Ссылка на объявление` VARCHAR(255), `Местоположение` VARCHAR(255), `Ссылка на изображение` VARCHAR(255), `Имя продавца` VARCHAR(255), `Номер продавца` VARCHAR(255), `Количество объявлений продавца` VARCHAR(255), `Дата регистрации` VARCHAR(255) DEFAULT NULL, `Бизнесс аккаунт` VARCHAR(255) DEFAULT NULL)", (tb_name, ))
 		return
 
 
 	def add_hash_advertisement(self, user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business):
-		sql = "INSERT INTO `{}` (`Площадка`, `Название объявления`, `Цена объявления`, `Дата создания объявления`, `Ссылка на объявление`, `Местоположение`, `Ссылка на изображение`, `Имя продавца`, `Номер продавца`, `Количество объявлений продавца`, `Дата регистрации`, `Бизнесс аккаунт`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)".format(user_id)
-		val = platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business
+		sql = "INSERT INTO `%s` (`Площадка`, `Название объявления`, `Цена объявления`, `Дата создания объявления`, `Ссылка на объявление`, `Местоположение`, `Ссылка на изображение`, `Имя продавца`, `Номер продавца`, `Количество объявлений продавца`, `Дата регистрации`, `Бизнесс аккаунт`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+		val = user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business
 		self.hash_cursor.execute(sql, val)
 		return self.hash_mydb.commit()
 
 
 	def get_hash_data(self, user_id):
 	# """Получаем все элементы ВРЕМЕННОЙ бд"""
-		sql = "SELECT * FROM `{}`".format(user_id)
-		self.hash_cursor.execute(sql)
+		self.hash_cursor.execute("SELECT * FROM `%s`", (user_id, ))
 		return self.hash_cursor.fetchall()
 
 	def clear_hash_data(self, tb_name):
-		sql = "TRUNCATE TABLE `{}`".format(tb_name)
-		return self.hash_cursor.execute(sql)
+		return self.hash_cursor.execute("TRUNCATE TABLE `%s`", (tb_name, ))
 
 
 	def len_hash_data(self, user_id):
 	# """Получаем длину ВРЕМЕННОЙ бд"""
-		sql = "SELECT * FROM `{}`".format(user_id)
-		self.hash_cursor.execute(sql)
+		self.hash_cursor.execute("SELECT * FROM `%s`", (user_id, ))
 		length = len(self.hash_cursor.fetchall())
 		return length
 
 
 	def delete_table(self, tb_name):
-		sql = "DROP TABLE `{}`".format(tb_name)
-		return self.adv_cursor.execute(sql)
+		return self.adv_cursor.execute("DROP TABLE `%s`", (tb_name, ))
 
 	def delete_hash_table(self, tb_name):
-		sql = "DROP TABLE `{}`".format(tb_name)
-		return self.hash_cursor.execute(sql)
+		return self.hash_cursor.execute("DROP TABLE `%s`", (tb_name, ))
 
 	# def delete_temporary_params_table(self, tb_name):
 	# 	sql = "DROP TABLE `{}`".format(tb_name)
 	# 	return self.temporaryparams_cursor.execute(sql)
 
 	def delete_users_table(self, tb_name):
-		sql = "DROP TABLE `{}`".format(tb_name)
-		return self.user_cursor.execute(sql)
+		sql = "DROP TABLE `%s`"
+		return self.user_cursor.execute(sql, tb_name)
 
 	def delete_users_subsc_database(self, country):
 		sql = "DROP DATABASE `{}`".format(country)
@@ -232,29 +220,26 @@ class SQLighter:
 
 
 # bolhasi
-	def create_countries_sub(self):
-		db = "CREATE DATABASE `{}`".format('countries_subscribers')
-		return self.adv_cursor.execute(db)
+	# def create_countries_sub(self):
+	# 	db = "CREATE DATABASE `{}`".format('countries_subscribers')
+	# 	return self.adv_cursor.execute(db)
 
 	def create_countries_sub_table(self, country):
-		countrysub_table = "CREATE TABLE `{}` (`ID` INT AUTO_INCREMENT PRIMARY KEY, `user_id` VARCHAR(255), `time_until` VARCHAR(255))".format(country)
-		return self.countriessub_cursor.execute(countrysub_table)
+		return self.countriessub_cursor.execute("CREATE TABLE `%s` (`ID` INT AUTO_INCREMENT PRIMARY KEY, `user_id` VARCHAR(255), `time_until` VARCHAR(255))", (country, ))
 
 	def add_subscriber(self, country, user_id, time_until = datetime.now()):
-		sql = "INSERT INTO `{}` (`user_id`, `time_until`) VALUES (%s,%s)".format(country)
-		val = user_id, time_until
+		sql = "INSERT INTO `%s` (`user_id`, `time_until`) VALUES (%s,%s)"
+		val = country, user_id, time_until
 		self.countriessub_cursor.execute(sql, val)
 		return self.countriessub_mydb.commit()
 
 	def check_subscriber(self, country, user_id):
-		sql = "SELECT * FROM `{0}` WHERE user_id = {1}".format(country, user_id)
-		self.countriessub_cursor.execute(sql)
+		self.countriessub_cursor.execute("SELECT * FROM `%s` WHERE user_id = %s", (country, user_id,))
 		data = self.countriessub_cursor.fetchone()
 		return data
 
 	def get_subscriber_time(self, country, user_id):
-		sql = "SELECT time_until FROM `{0}` WHERE user_id = {1}".format(country, user_id)
-		self.countriessub_cursor.execute(sql)
+		self.countriessub_cursor.execute("SELECT time_until FROM `%s` WHERE user_id = %s", (country, user_id,))
 		sub_time = self.countriessub_cursor.fetchone()
 		a = sub_time[0]
 		b = a.split("-")
@@ -265,16 +250,15 @@ class SQLighter:
 
 
 	def update_subsc_time(self, user_id, time_until, country):
-		sql = "UPDATE `{}` SET time_until = %s WHERE user_id = %s".format(country)
-		val = (time_until, user_id)
+		sql = "UPDATE `%s` SET time_until = %s WHERE user_id = %s"
+		val = (country, time_until, user_id)
 		self.countriessub_cursor.execute(sql, val)
 		self.countriessub_mydb.commit()
 
 
-	def clear_sub_data(self, country):
-		sql = "TRUNCATE TABLE `{}`".format(country)
-		return self.countriessub_cursor.execute(sql)
-
+	# def clear_sub_data(self, country):
+	# 	sql = "TRUNCATE TABLE `{}`".format(country)
+	# 	return self.countriessub_cursor.execute(sql)
 
 
 if __name__ == '__main__':
