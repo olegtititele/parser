1e9d09b3985453279f0bae8847587897d313a2c9
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-20 14:20:04 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,14 +1,25 @@
+BOT_TOKEN = "5057048367:AAHWkgCfcL0_rvJap4-NWL-tshtSIETya-Y"
+
+
+# Канал Новостей
 CHANNEL = "https://t.me/gzuzpars"
 CHANNEL_CHAT_ID = -1001566638816
+
+
+# Админ канал
 ADMIN_LOGS_CHAT_ID = -746110313
 COUNTRIES_SITES = {
 				'🇸🇮Словения🇸🇮':['bolha.com'],
 				'🇱🇺Люксембург🇱🇺':['bazar.lu'],
 				}
 
+
+
 PRICES = {'🛒 Купить на 1 день':1, '🛒 Купить на 3 дня':3, '🛒 Купить на 7 дней':7, '🛒 Купить на 30 дней':30}
 
-BOT_TOKEN = "5057048367:AAHWkgCfcL0_rvJap4-NWL-tshtSIETya-Y"
+ADMIN_COMMANDS = {'/get_user_length':'получить количество пользователей бота'}
+
+
 # PROXY = 'Selhdfhvhhxfhhg:Y6f4XpG@191.101.148.36:45786'
 
 # db = ['185.186.2.211', '6033', 'bluefasick', '3at116lKfbo3']
