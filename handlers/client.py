1ab42fca48c69fe80458eb1878bba9034e5a73c6
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-20 14:31:09 +0300

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -6,7 +6,7 @@ from keyboards import *
 
 
 db = SQLighter()
-
+print(str(db.get_users_length()))
 def check_sub_channel(chat_member):
 	if chat_member['status'] != 'left':
 		return True
@@ -61,10 +61,11 @@ async def help_command(message: types.Message):
 
 async def get_users_length(message: types.Message):
 	if check_sub_channel(await bot.get_chat_member(chat_id=cf.ADMIN_LOGS_CHAT_ID, user_id=message.from_user.id)):
-		await bot.send_message(
-			chat_id=cf.ADMIN_LOGS_CHAT_ID,
-			text="Количество пользователей бота: " +str(db.get_users_length()),
-			parse_mode=types.ParseMode.HTML)
+		print("dada")
+# 		await bot.send_message(
+# 			chat_id=cf.ADMIN_LOGS_CHAT_ID,
+# 			text="Количество пользователей бота: " +str(db.get_users_length()),
+# 			parse_mode=types.ParseMode.HTML)
 	else:
 		pass
 
