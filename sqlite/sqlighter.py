688b85680fe95fe396cd1156467cf527ab35f9ff
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-21 16:27:56 +0300

    Update sqlighter.py

--- sqlite/sqlighter.py
+++ sqlite/sqlighter.py
@@ -76,29 +76,41 @@ class SQLighter:
 
 
 	def add_user(self, user_id, username):
-		sql = "INSERT INTO `users` (`user_id`, `username`) VALUES (%s,%s)"
-		val = user_id, username
-		self.user_cursor.execute(sql, val)
-		self.user_mydb.commit()
-		return
+		try:
+			sql = "INSERT INTO `users` (`user_id`, `username`) VALUES (%s,%s)"
+			val = user_id, username
+			self.user_cursor.execute(sql, val)
+			self.user_mydb.commit()
+			return
+		except Exception as e:
+			return	
 
 	def check_user(self, user_id):
-		self.user_cursor.execute("SELECT * FROM users WHERE `user_id` = %s", (user_id, ))
-		data = self.user_cursor.fetchone()
-		return data
+		try:
+			self.user_cursor.execute("SELECT * FROM users WHERE `user_id` = %s", (user_id, ))
+			data = self.user_cursor.fetchone()
+			return data
+		except Exception as e:
+			return	
 
 
 	def get_users_data(self):
-		sql = "SELECT * FROM users"
-		self.user_cursor.execute(sql)
-		return self.user_cursor.fetchall()
+		try:
+			sql = "SELECT * FROM users"
+			self.user_cursor.execute(sql)
+			return self.user_cursor.fetchall()
+		except Exception as e:
+			return	
 
 
 	def get_users_length(self):
-		sql = "SELECT * FROM users"
-		self.user_cursor.execute(sql)
-		length = len(self.user_cursor.fetchall())
-		return length
+		try:
+			sql = "SELECT * FROM users"
+			self.user_cursor.execute(sql)
+			length = len(self.user_cursor.fetchall())
+			return length
+		except Exception as e:
+			return		
 
 	# def clear_users_table(self, user_id):
 	# 	sql = "TRUNCATE TABLE `{}`".format(user_id)
@@ -110,113 +122,167 @@ class SQLighter:
 # """Действия с основной бд"""
 
 	def create_advertisement_table(self, user_id):
-		self.adv_cursor.execute("CREATE TABLE `%s` (`Площадка` VARCHAR(255), `Название объявления` VARCHAR(255), `Цена объявления` VARCHAR(255), `Дата создания объявления` VARCHAR(255), `Ссылка на объявление` VARCHAR(255), `Местоположение` VARCHAR(255), `Ссылка на изображение` VARCHAR(255), `Имя продавца` VARCHAR(255), `Номер продавца` VARCHAR(255), `Количество объявлений продавца` VARCHAR(255), `Дата регистрации` VARCHAR(255) DEFAULT NULL, `Бизнесс аккаунт` VARCHAR(255) DEFAULT NULL)", (user_id))
-		return
+		try:
+			self.adv_cursor.execute("CREATE TABLE `%s` (`Площадка` VARCHAR(255), `Название объявления` VARCHAR(255), `Цена объявления` VARCHAR(255), `Дата создания объявления` VARCHAR(255), `Ссылка на объявление` VARCHAR(255), `Местоположение` VARCHAR(255), `Ссылка на изображение` VARCHAR(255), `Имя продавца` VARCHAR(255), `Номер продавца` VARCHAR(255), `Количество объявлений продавца` VARCHAR(255), `Дата регистрации` VARCHAR(255) DEFAULT NULL, `Бизнесс аккаунт` VARCHAR(255) DEFAULT NULL)", (user_id))
+			return
+		except Exception as e:
+			return		
 
 
 	def add_advertisement(self, user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business):
-		sql = "INSERT INTO `%s` (`Площадка`, `Название объявления`, `Цена объявления`, `Дата создания объявления`, `Ссылка на объявление`, `Местоположение`, `Ссылка на изображение`, `Имя продавца`, `Номер продавца`, `Количество объявлений продавца`, `Дата регистрации`, `Бизнесс аккаунт`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-		val = user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business
-		self.adv_cursor.execute(sql, val)
-		self.adv_mydb.commit()
-		return
+		try:
+			sql = "INSERT INTO `%s` (`Площадка`, `Название объявления`, `Цена объявления`, `Дата создания объявления`, `Ссылка на объявление`, `Местоположение`, `Ссылка на изображение`, `Имя продавца`, `Номер продавца`, `Количество объявлений продавца`, `Дата регистрации`, `Бизнесс аккаунт`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+			val = user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business
+			self.adv_cursor.execute(sql, val)
+			self.adv_mydb.commit()
+			return
+		except Exception as e:
+			return		
 
 
 	def get_len_previously_platfrom(self, user_id, platform):
-		sql = "SELECT * FROM `%s` WHERE `Площадка`=%s"
-		adr = (user_id, platform, )
-		self.adv_cursor.execute(sql, adr)
-		length = len(self.adv_cursor.fetchall())
-		return length
+		try:
+			sql = "SELECT * FROM `%s` WHERE `Площадка`=%s"
+			adr = (user_id, platform, )
+			self.adv_cursor.execute(sql, adr)
+			length = len(self.adv_cursor.fetchall())
+			return length
+		except Exception as e:
+			return	
 
 	def get_previously_adv(self, user_id, platform):
-		sql = "SELECT * FROM `%s` WHERE `Площадка`=%s"
-		adr = (user_id, platform, )
-		self.adv_cursor.execute(sql, adr)
-		data = self.adv_cursor.fetchall()
-		return data
+		try:
+			sql = "SELECT * FROM `%s` WHERE `Площадка`=%s"
+			adr = (user_id, platform, )
+			self.adv_cursor.execute(sql, adr)
+			data = self.adv_cursor.fetchall()
+			return data
+		except Exception as e:
+			return	
 
 	def delete_previously_adv(self, user_id, platform):
-		sql = "DELETE FROM `%s` WHERE `Площадка`=%s"
-		adr = (user_id, platform, )
-		self.adv_cursor.execute(sql, adr)
-		self.adv_mydb.commit()
-		return 
+		try:
+			sql = "DELETE FROM `%s` WHERE `Площадка`=%s"
+			adr = (user_id, platform, )
+			self.adv_cursor.execute(sql, adr)
+			self.adv_mydb.commit()
+			return
+		except Exception as e:
+			return	
 		
 
 
 	def len_advertisement_data(self, user_id):
-		self.adv_cursor.execute("SELECT * FROM `%s`", (user_id,))
-		length = len(self.adv_cursor.fetchall())
-		print(length)
-		return length
+		try:
+			self.adv_cursor.execute("SELECT * FROM `%s`", (user_id,))
+			length = len(self.adv_cursor.fetchall())
+			print(length)
+			return length
+		except Exception as e:
+			return	
 
 
 	def check_advestisement(self, user_id, adv_url):
-		sql = "SELECT * FROM `%s` WHERE `Ссылка на объявление`=%s"
-		adr = (user_id, adv_url, )
-		self.adv_cursor.execute(sql, adr)
-		data = self.adv_cursor.fetchone()
-		return data
+		try:
+			sql = "SELECT * FROM `%s` WHERE `Ссылка на объявление`=%s"
+			adr = (user_id, adv_url, )
+			self.adv_cursor.execute(sql, adr)
+			data = self.adv_cursor.fetchone()
+			return data
+		except Exception as e:
+			return	
 
 	def get_tel_num(self, user_id, telnum):
-		sql = "SELECT * FROM `%s` WHERE `Номер продавца`=%s"
-		adr = (user_id, telnum, )
-		self.adv_cursor.execute(sql, adr)
-		data = self.adv_cursor.fetchone()
-		return data	
+		try:
+			sql = "SELECT * FROM `%s` WHERE `Номер продавца`=%s"
+			adr = (user_id, telnum, )
+			self.adv_cursor.execute(sql, adr)
+			data = self.adv_cursor.fetchone()
+			return data
+		except Exception as e:
+			return	
 
 	def clear_advestisement(self, user_id):
-		return self.adv_cursor.execute("TRUNCATE TABLE `%s`", (user_id,))
+		try:
+			return self.adv_cursor.execute("TRUNCATE TABLE `%s`", (user_id,))
+		except Exception as e:
+			return		
 
 
 # """Действия с временной бд"""
 
 	def create_hash_table(self, tb_name):
-		self.hash_cursor.execute("CREATE TABLE `%s` (`Площадка` VARCHAR(255), `Название объявления` VARCHAR(255), `Цена объявления` VARCHAR(255), `Дата создания объявления` VARCHAR(255), `Ссылка на объявление` VARCHAR(255), `Местоположение` VARCHAR(255), `Ссылка на изображение` VARCHAR(255), `Имя продавца` VARCHAR(255), `Номер продавца` VARCHAR(255), `Количество объявлений продавца` VARCHAR(255), `Дата регистрации` VARCHAR(255) DEFAULT NULL, `Бизнесс аккаунт` VARCHAR(255) DEFAULT NULL)", (tb_name, ))
-		return
+		try:
+			self.hash_cursor.execute("CREATE TABLE `%s` (`Площадка` VARCHAR(255), `Название объявления` VARCHAR(255), `Цена объявления` VARCHAR(255), `Дата создания объявления` VARCHAR(255), `Ссылка на объявление` VARCHAR(255), `Местоположение` VARCHAR(255), `Ссылка на изображение` VARCHAR(255), `Имя продавца` VARCHAR(255), `Номер продавца` VARCHAR(255), `Количество объявлений продавца` VARCHAR(255), `Дата регистрации` VARCHAR(255) DEFAULT NULL, `Бизнесс аккаунт` VARCHAR(255) DEFAULT NULL)", (tb_name, ))
+			return
+		except Exception as e:
+			return		
 
 
 	def add_hash_advertisement(self, user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business):
-		sql = "INSERT INTO `%s` (`Площадка`, `Название объявления`, `Цена объявления`, `Дата создания объявления`, `Ссылка на объявление`, `Местоположение`, `Ссылка на изображение`, `Имя продавца`, `Номер продавца`, `Количество объявлений продавца`, `Дата регистрации`, `Бизнесс аккаунт`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-		val = user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business
-		self.hash_cursor.execute(sql, val)
-		return self.hash_mydb.commit()
+		try:
+			sql = "INSERT INTO `%s` (`Площадка`, `Название объявления`, `Цена объявления`, `Дата создания объявления`, `Ссылка на объявление`, `Местоположение`, `Ссылка на изображение`, `Имя продавца`, `Номер продавца`, `Количество объявлений продавца`, `Дата регистрации`, `Бизнесс аккаунт`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+			val = user_id, platform, adv_name, adv_price, adv_reg, adv_url, location, adv_image_url, seller_name, seller_tel_number, seller_count_adv, seller_reg_data, check_business
+			self.hash_cursor.execute(sql, val)
+			return self.hash_mydb.commit()
+		except Exception as e:
+			return	
 
 
 	def get_hash_data(self, user_id):
 	# """Получаем все элементы ВРЕМЕННОЙ бд"""
-		self.hash_cursor.execute("SELECT * FROM `%s`", (user_id, ))
-		return self.hash_cursor.fetchall()
+		try:
+			self.hash_cursor.execute("SELECT * FROM `%s`", (user_id, ))
+			return self.hash_cursor.fetchall()
+		except Exception as e:
+			return	
 
 	def clear_hash_data(self, tb_name):
-		return self.hash_cursor.execute("TRUNCATE TABLE `%s`", (tb_name, ))
+		try:
+			return self.hash_cursor.execute("TRUNCATE TABLE `%s`", (tb_name, ))
+		except Exception as e:
+			return	
 
 
 	def len_hash_data(self, user_id):
 	# """Получаем длину ВРЕМЕННОЙ бд"""
-		self.hash_cursor.execute("SELECT * FROM `%s`", (user_id, ))
-		length = len(self.hash_cursor.fetchall())
-		return length
+		try:
+			self.hash_cursor.execute("SELECT * FROM `%s`", (user_id, ))
+			length = len(self.hash_cursor.fetchall())
+			return length
+		except Exception as e:
+			return	
 
 
 	def delete_table(self, tb_name):
-		return self.adv_cursor.execute("DROP TABLE `%s`", (tb_name, ))
+		try:
+			return self.adv_cursor.execute("DROP TABLE `%s`", (tb_name, ))
+		except Exception as e:
+			return		
 
 	def delete_hash_table(self, tb_name):
-		return self.hash_cursor.execute("DROP TABLE `%s`", (tb_name, ))
+		try:
+			return self.hash_cursor.execute("DROP TABLE `%s`", (tb_name, ))
+		except Exception as e:
+			return	
 
 	# def delete_temporary_params_table(self, tb_name):
 	# 	sql = "DROP TABLE `{}`".format(tb_name)
 	# 	return self.temporaryparams_cursor.execute(sql)
 
 	def delete_users_table(self, tb_name):
-		sql = "DROP TABLE `%s`"
-		return self.user_cursor.execute(sql, tb_name)
+		try:
+			sql = "DROP TABLE `%s`"
+			return self.user_cursor.execute(sql, tb_name)
+		except Exception as e:
+			return	
 
 	def delete_users_subsc_database(self, country):
-		sql = "DROP DATABASE `{}`".format(country)
-		return self.adv_cursor.execute(sql)	
+		try:
+			sql = "DROP DATABASE `{}`".format(country)
+			return self.adv_cursor.execute(sql)
+		except Exception as e:
+			return	
 
 
 # bolhasi
@@ -225,61 +291,52 @@ class SQLighter:
 	# 	return self.adv_cursor.execute(db)
 
 	def create_countries_sub_table(self, country):
-		return self.countriessub_cursor.execute("CREATE TABLE `%s` (`ID` INT AUTO_INCREMENT PRIMARY KEY, `user_id` VARCHAR(255), `time_until` VARCHAR(255))", (country, ))
+		try:
+			return self.countriessub_cursor.execute("CREATE TABLE `%s` (`ID` INT AUTO_INCREMENT PRIMARY KEY, `user_id` VARCHAR(255), `time_until` VARCHAR(255))", (country, ))
+		except Exception as e:
+			return	
 
 	def add_subscriber(self, country, user_id, time_until = datetime.now()):
-		self.countriessub_cursor.execute("INSERT INTO `%s` (`user_id`, `time_until`) VALUES (%s,%s)", (country, user_id, time_until,))
-		return self.countriessub_mydb.commit()
+		try:
+			self.countriessub_cursor.execute("INSERT INTO `%s` (`user_id`, `time_until`) VALUES (%s,%s)", (country, user_id, time_until,))
+			return self.countriessub_mydb.commit()
+		except Exception as e:
+			return	
 
 	def check_subscriber(self, country, user_id):
-		sql = "SELECT * FROM `%s` WHERE `user_id` = %s"
-		adr = (country, user_id,)
-		self.countriessub_cursor.execute(sql, adr)
-		data = self.countriessub_cursor.fetchone()
-		return data
+		try:
+			sql = "SELECT * FROM `%s` WHERE `user_id` = %s"
+			adr = (country, user_id,)
+			self.countriessub_cursor.execute(sql, adr)
+			data = self.countriessub_cursor.fetchone()
+			return data
+		except Exception as e:
+			return	
 
 	def get_subscriber_time(self, country, user_id):
-		self.countriessub_cursor.execute("SELECT time_until FROM `%s` WHERE `user_id` = %s", (country, user_id,))
-		sub_time = self.countriessub_cursor.fetchone()
-		a = sub_time[0]
-		b = a.split("-")
-		c = b[2].split(" ")
-		d = c[1].split(":")
-		k = d[2].split(".")
-		return datetime(int(b[0]), int(b[1]), int(c[0]), int(d[0]), int(d[1]), int(k[0]), int(k[1]))
+		try:
+			self.countriessub_cursor.execute("SELECT time_until FROM `%s` WHERE `user_id` = %s", (country, user_id,))
+			sub_time = self.countriessub_cursor.fetchone()
+			a = sub_time[0]
+			b = a.split("-")
+			c = b[2].split(" ")
+			d = c[1].split(":")
+			k = d[2].split(".")
+			return datetime(int(b[0]), int(b[1]), int(c[0]), int(d[0]), int(d[1]), int(k[0]), int(k[1]))
+		except Exception as e:
+			return	
 
 
 	def update_subsc_time(self, user_id, time_until, country):
-		self.countriessub_cursor.execute("UPDATE `%s` SET `time_until` = %s WHERE `user_id` = %s", (country, time_until, user_id,))
-		self.countriessub_mydb.commit()
+		try:
+			self.countriessub_cursor.execute("UPDATE `%s` SET `time_until` = %s WHERE `user_id` = %s", (country, time_until, user_id,))
+			self.countriessub_mydb.commit()
+		except Exception as e:
+			return	
 
 	def drop_drop(self, country):
-		self.countriessub_cursor.execute("TRUNCATE TABLE `%s`", (country, ))
-		self.countriessub_mydb.commit()
-
-
-if __name__ == '__main__':
-	sq = SQLighter()
-	# sq.create_users_db()
-	# sq.delete_users_subsc_database()
-	# sq.clear_sub_data("bazar.lu")
-	# sq.create_countries_sub()
-	# sq.create_countries_sub_table("bolha.com")
-	# sq.create_countries_sub_table("bazar.lu")
-	# sq.delete_users_subsc_database('countri1es_subscribers')
-	# sq.create_countries_sub_table("bazar.lu")
-	# sq.delete_users_table('users')
-	# sq.create_users_table()
-	# sq.clear_hash_data(2029023685)
-	# sq.clear_advestisement(2029023685)
-	# sq.add_temporary_params(2029023685, "bolha.si", 'https://www.bolha.com/index.php?ctl=search_ads&keywords=apple', '25', 'нет', '21.03.2015', 'нет', 'нет')
-	# sq.create_temporary_params_table(2029023685)
-	# sq.delete_users_table('usersdb')
-	# sq.delete_table(2029023685)
-	# sq.delete_hash_table(2029023685)
-	# sq.delete_temporary_params_table(2029023685)
-	# sq.create_users_table()
-# 	sq.get_advertisement_data(2029023685, "bolha.com")
-	# for i in sq.get_advertisement_data(2029023685):
-	# 	if i[0] == "bolha.com":
-
+		try:
+			self.countriessub_cursor.execute("TRUNCATE TABLE `%s`", (country, ))
+			self.countriessub_mydb.commit()
+		except Exception as e:
+			return	
