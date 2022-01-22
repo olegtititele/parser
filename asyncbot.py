7d109e9e5e819d2dca73c1dc298939be195bc106
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-23 00:57:37 +0300

    Update asyncbot.py

--- asyncbot.py
+++ asyncbot.py
@@ -303,10 +303,10 @@ async def start_pars_button1(call: types.CallbackQuery, state: FSMContext):
 # BOLHA.COM
 	if platform == "bolha.com":
 		if call.data == 'start_oars':
-		while True:
-			print(call.from_user.id)
-			if call.data == 'stop_parser':
-				break
+			while True:
+				print(call.from_user.id)
+				if call.data == 'stop_parser':
+					break
 # 		bolha = BolhaSI(call.from_user.id, platform, link, adv_count, seller_adv, adv_reg_data, reg_seller_data, business, repeated_number)
 # 		dyn_load = DynamicLoading(call.from_user.id)
 # # 		thread_pars = Thread(target=bolha.generate_link)
