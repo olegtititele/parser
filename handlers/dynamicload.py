24f3f19a0ec6d52eb6dc95cd060fe7e8ea8105a0
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-22 20:59:07 +0300

    Update dynamicload.py

--- handlers/dynamicload.py
+++ handlers/dynamicload.py
@@ -44,7 +44,9 @@ class DynamicLoading(object):
 				print(call.data)
 				db = SQLighter()
 				length = str(db.len_hash_data(call.from_user.id))
-				if int(length) == total_adv or self.loopflag == False:
+				if self.loopflag == False:
+					break
+				elif int(length) == total_adv:
 					if length[-1] == "1" and length != "11":
 						line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявление из "+ str(total_adv) + "</b>"
 					elif length[-1] in ("2", "3", "4") and length != "12" and length != "13" and length != "14":
@@ -53,6 +55,7 @@ class DynamicLoading(object):
 						line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявлений из "+ str(total_adv) + "</b>"
 					await call.message.edit_text(text=line, parse_mode=types.ParseMode.HTML, reply_markup=just_parsed_kb)
 					await state.finish()
+					self.loopflag == False
 					break
 				else:
 					load = "<b>" + i + " Поиск объявлений в процессе</b>"
