ac7f207743d6d04555e9ce6da9d0c1b2521dd9f8
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-23 14:17:53 +0300

    Update dynamicload.py

--- handlers/dynamicload.py
+++ handlers/dynamicload.py
@@ -33,7 +33,6 @@ class DynamicLoading(object):
 			line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявлений из "+ str(total_adv) + "</b>"
 		await call.message.edit_text(text=line, parse_mode=types.ParseMode.HTML, reply_markup=just_parsed_kb)
 		await state.finish()
-		time.sleep(60)
 
 	async def start_loop(self, call, state: FSMContext):
 # 		global loopflag
