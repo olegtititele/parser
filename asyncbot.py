98e0045ec3ea5f91157d422d5df33b504ac957b3
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-23 17:54:16 +0300

    Update asyncbot.py

--- asyncbot.py
+++ asyncbot.py
@@ -319,7 +319,7 @@ async def start_pars_button1(call: types.CallbackQuery, state: FSMContext):
 		if call.data == 'start_pars':
 			await call.message.edit_text(text="⌛️ Поиск объявлений начался. Это займет несколько минут.", parse_mode=types.ParseMode.HTML)
 # 			thread_pars.start()
-			dp.loop.create_task(await dyn_load.start_loop(call, state))
+			asyncio.create_task(await dyn_load.start_loop(call, state))
 
 		elif call.data == "stop_parser"+str(call.from_user.id):
 			print("Вышел")
