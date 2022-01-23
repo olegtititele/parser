20bd5746b76e01268c2329999eda1d1c6b273677
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-23 14:29:14 +0300

    Update dynamicload.py

--- handlers/dynamicload.py
+++ handlers/dynamicload.py
@@ -15,24 +15,26 @@ class DynamicLoading(object):
 	def __init__(self):
 # 		self.user_id = user_id
 		self.loopflag = True
-		self.a = []
+# 		self.iy = 0
 		
 	async def stop_loop(self, call, state: FSMContext):
 # 		global loopflag
 # 		loopflag = 'False'+str(call.from_user.id)
 		self.a.append(call.from_user.id)
-		async with state.proxy() as data:
-			total_adv = data['adv_count']
-		db = SQLighter()
-		length = str(db.len_hash_data(call.from_user.id))	
-		if length[-1] == "1" and length != "11":
-			line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявление из "+ str(total_adv) + "</b>"
-		elif length[-1] in ("2", "3", "4") and length != "12" and length != "13" and length != "14":
-			line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявления из "+ str(total_adv) + "</b>"
-		elif length[-1] in ("0","5", "6", "7", "8", "9") or length == "11" or length == "12" or length == "14":
-			line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявлений из "+ str(total_adv) + "</b>"
-		await call.message.edit_text(text=line, parse_mode=types.ParseMode.HTML, reply_markup=just_parsed_kb)
-		await state.finish()
+		global iy
+		iy = 20
+# 		async with state.proxy() as data:
+# 			total_adv = data['adv_count']
+# 		db = SQLighter()
+# 		length = str(db.len_hash_data(call.from_user.id))	
+# 		if length[-1] == "1" and length != "11":
+# 			line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявление из "+ str(total_adv) + "</b>"
+# 		elif length[-1] in ("2", "3", "4") and length != "12" and length != "13" and length != "14":
+# 			line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявления из "+ str(total_adv) + "</b>"
+# 		elif length[-1] in ("0","5", "6", "7", "8", "9") or length == "11" or length == "12" or length == "14":
+# 			line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявлений из "+ str(total_adv) + "</b>"
+# 		await call.message.edit_text(text=line, parse_mode=types.ParseMode.HTML, reply_markup=just_parsed_kb)
+# 		await state.finish()
 
 	async def start_loop(self, call, state: FSMContext):
 # 		global loopflag
@@ -43,13 +45,12 @@ class DynamicLoading(object):
 		stop_kb = InlineKeyboardMarkup()
 		stop_kb.add(InlineKeyboardButton(text="❌ Остановить парсер", callback_data=stop_btn))
 		coursor = '🌕🌖🌗🌘🌑🌒🌓🌔'
+		global iy
 		iy = 0
 		while self.loopflag:	
 			for i in coursor:
 				db = SQLighter()
 				length = str(db.len_hash_data(call.from_user.id))
-				iy +=1
-				print(iy)
 				if int(length) == total_adv or iy == 20:
 					if length[-1] == "1" and length != "11":
 						line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявление из "+ str(total_adv) + "</b>"
