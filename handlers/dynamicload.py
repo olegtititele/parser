1379fa77f979c9d140c2389027ee9d566c05b1d6
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-23 20:15:56 +0300

    Update dynamicload.py

--- handlers/dynamicload.py
+++ handlers/dynamicload.py
@@ -31,7 +31,7 @@ class DynamicLoading(object):
 					elif length[-1] in ("0","5", "6", "7", "8", "9") or length == "11" or length == "12" or length == "14":
 						line = "✅<b>Поиск объявлений завершен. Получено "+length+ " объявлений из "+ str(total_adv) + "</b>"
 					await state.finish()
-					return await call.message.edit_text(text=line, parse_mode=types.ParseMode.HTML)
+					return await call.message.edit_text(text=line, parse_mode=types.ParseMode.HTML, reply_markup=just_parsed_kb)
 				else:
 					load = "<b>" + i + " Поиск объявлений в процессе</b>"
 					if length[-1] == "1" and length != "11":
@@ -40,5 +40,5 @@ class DynamicLoading(object):
 						line = "<b>Получено "+length+ " объявления из "+ str(total_adv) + "</b>"
 					elif length[-1] in ("0","5", "6", "7", "8", "9") or length == "11" or length == "12" or length == "14":
 						line = "<b>Получено "+length+ " объявлений из "+ str(total_adv) + "</b>"
-					await call.message.edit_text(text=load+"\n\n"+line, parse_mode=types.ParseMode.HTML, reply_markup=stop_kb)
+					await call.message.edit_text(text=load+"\n\n"+line, parse_mode=types.ParseMode.HTML)
 					time.sleep(0.5)
