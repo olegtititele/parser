7bf81e535501cd48449a2a3e95df292faba889ac
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-01-26 20:53:49 +0300

    Update gumtreecoza.py

--- countries/gumtreecoza.py
+++ countries/gumtreecoza.py
@@ -85,7 +85,7 @@ class GumtreeCoZa(object):
 			self.driver.get(adv_link)
 			time.sleep(1)
 			element = self.driver.find_element(By.XPATH, '//*[@id="reply-form"]/div/div[2]/div[1]/div/span[3]')
-			print("ssss")
+			print(element)
 			element.click()
 			phone_number = "+27" + self.driver.find_element(By.XPATH, '//*[@id="reply-form"]/div/div[2]/div[1]/div/span[2]').text.replace("-", "")
 			print(phone_number)
